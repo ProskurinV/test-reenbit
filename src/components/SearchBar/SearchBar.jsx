6120bdba52e95bf90fb4fa68212ac14a65bc5b96
ProskurinV <proskyrinv.g@gmail.com>
2023-03-14 19:08:41 +0200

    local

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Wrapper, Input, Icon } from './SearchBar.styled';
 
 export const SearchBar = ({ query, setQuery }) => {
+  useEffect(() => {
+    const storedQuery = localStorage.getItem('query');
+    if (storedQuery) {
+      setQuery(storedQuery);
+    }
+  }, [setQuery]);
+
+  const handleChange = event => {
+    const newQuery = event.target.value;
+    setQuery(newQuery);
+    localStorage.setItem('query', newQuery);
+  };
+
   return (
     <Wrapper>
       <Icon />
       <Input
         type="text"
         value={query}
-        onChange={event => setQuery(event.target.value)}
+        onChange={handleChange}
         placeholder="Filter by name..."
       />
     </Wrapper>
