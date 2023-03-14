60defd71ec0994d49b89fb920abac7ac47c7d566
ProskurinV <proskyrinv.g@gmail.com>
2023-03-14 20:34:08 +0200

    pathMove

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
+import { Suspense, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import { CharactersList } from '../../components/CharactersList/CharacterLists';
 import { Loader } from '../../components/Loader/Loader';
 import { Container, Header } from './Home.styled';
 import { SearchBar } from '../../components/Searchbar/SearchBar';
-import { Suspense, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+
 import { useAllCharacters } from '../../hooks/useAllCharacters';
 
 const Home = () => {
